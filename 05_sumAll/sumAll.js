41af46c9d6b713510962750093fec5c3a3008317
ArsenLupen53 <kingnocktis@gmail.com>
2023-08-07 19:34:07 +0300

    ABOUT TO FINISH

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
-
+function sumAll(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+      return 'ERROR';
+    }
+  
+    if (start < 0 || end < 0) {
+      return 'ERROR';
+    }
+  
+    let sum = 0;
+    for (let i = Math.min(start, end); i <= Math.max(start, end); i++) {
+      sum += i;
+    }
+  
+    return sum;
+  }
 // Do not edit below this line
 module.exports = sumAll;
