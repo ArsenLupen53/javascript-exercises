214e3dd7c5632f83c9b539f3d32ae74c333896ef
ArsenLupen53 <kingnocktis@gmail.com>
2023-08-16 18:10:50 +0300

    döne

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,17 @@
-const findTheOldest = function(args) {
-
-    args.map
+const findTheOldest = function(array) {
+    return array.reduce((oldest,currentPer) => {
+       const oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+       const currentAge = getAge(currentPer.yearOfBirth,currentPer.yearOfDeath);
+       return currentAge > oldestAge ? currentPer : oldest;
+    });
 };
+
+const getAge = function(birth,death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
